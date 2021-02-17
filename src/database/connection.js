bfe81e49ae4558bc586d29f55798538aa6e27b40
Ludriano Washington <ludinho.washi@gmail.com>
2021-02-17 00:08:25 -0300

    Heroku Deploy

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,6 @@
+const knex = require('knex')
+const configuration = require('../../knexfile')
 
-import knex from 'knex'
-import 'dotenv/config'
+const connection = knex(configuration.production)
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  useNullAsDefault: true
-})
-
-export default db
+module.exports = connection;
