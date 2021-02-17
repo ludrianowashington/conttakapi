a626f89802641a117b197da1f2d6f759098fe8ea
Ludriano Washington <ludinho.washi@gmail.com>
2021-02-16 23:24:56 -0300

    Making Heroku Deploy

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,49 @@
+// Update with your config settings.
+
+import path from 'path'
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  migrations: {
-    directory: 'src/database/migrations'
-  },
-  useNullAsDefault: true
-}
 
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./src/database/db.sqlite"
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
+  },
 
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+
+    client: "pg",
+    connection: {
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    },
+    migrations: {
+      directory: "./src/database/migrations"
+    },
+    useNullAsDefault: true
+  }
+
+};
