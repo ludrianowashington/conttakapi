6a8f93b51d8181a102fe91939fa1c87f7063b64c
Ludriano Washington <ludinho.washi@gmail.com>
2021-02-16 22:52:13 -0300

    Making Heroku Deploy

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,18 @@
+import path from 'path'
+import 'dotenv/config'
 
 module.exports = {
-
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./src/database/db.sqlite"
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
+  useNullAsDefault: true
+}
 
-  production: {
 
-    client: process.env.DATABASE_URL,
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    },
-    migrations: {
-      directory: "./src/database/migrations"
-    },
-    useNullAsDefault: true
-  }
-
-};
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,16 @@
-const knex = require('knex')
-const configuration = require('../../knexfile')
 
-const connection = knex(configuration.production)
+import knex from 'knex'
+import 'dotenv/config'
 
-module.exports = connection;
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+  },
+  useNullAsDefault: true
+})
+
+export default db
