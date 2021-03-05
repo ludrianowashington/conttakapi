e46e8ca370eac41e4e0bed26e6a5496ec4b05da6
Ludriano Washington <ludinho.washi@gmail.com>
2021-03-05 02:02:06 -0300

    atualizar dados

--- src/index.js
+++ src/index.js
@@ -5,23 +5,8 @@ const routes = require('./routes');
 
 const app = express();
 
-var allowedOrigins = ['http://localhost:3000', 'https://conttak.com'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
