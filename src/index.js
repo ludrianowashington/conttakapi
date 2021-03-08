e60871bede83804e8d419aa65a5250078b626c64
Ludriano Washington <ludinho.washi@gmail.com>
2021-03-08 13:26:19 -0300

    update back

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const routes = require('./routes');
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:3000',
-}
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
